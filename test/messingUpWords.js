39291546d3ca58e65dcced78d50bb99191326429
JEmerson23 <emersonbezerra174@gmail.com>
2022-11-18 12:36:58 -0300

    finalizando ultima função

--- test/messingUpWords.js
+++ test/messingUpWords.js
@@ -1,15 +1,22 @@
 /*
  DIA: 5
- objetivo: Função que receba uma string de uma ou mais palavras e retorne a mesma string,
-  mas com todas as palavras de cinco ou mais letras invertidas.
+ objetiv: Função que receba uma string de uma ou mais palavras e retorne a mesma string, mas com todas as palavras de cinco ou mais letras invertidas.
 */
 
-/*
+
 //[SUCESSO]
 function spinWords(str){
-   return str.split(' ').map(word => word.length >= 5?word.split('').reverse().join(''):word).join(' ');
+   return str.split(' ').map(word => word.length >= 5 ? word.split('').reverse().join(''):word).join(' ');
 }
 
 console.log(spinWords("Oi, batatas doce e doces de batata"));//ok -> Oi, satatab doce e secdo de atatab 
-*/
 
+
+/*
+//[SEM SUCESSO]
+//.reverce() não funciona
+const spinWords = str => str.split(' ')
+.map(word => word.length >= 5 ? word.split('').reverce().join('') : word).join(' ');
+
+console.log(spinWords("gorverno mundial"));
+*/
