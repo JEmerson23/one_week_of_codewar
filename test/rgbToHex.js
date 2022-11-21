dab802b91cd104b6368bbd8d5bb32e5517c60edc
JEmerson23 <emersonbezerra174@gmail.com>
2022-11-20 21:09:31 -0300

    criei a função, os testes na minha maquina funcionam, porem no codewars não

--- test/rgbToHex.js
+++ test/rgbToHex.js
@@ -5,5 +5,17 @@
 */
 
 function rgbToHex(r,g,b){
-  
+  let result = "";
+  for(let color of arguments){
+    if(color > 255)color = 255;
+    else if(color < 0)color = Math.abs(color);
+    
+    let colorInHex = color.toString(16);
+    result += colorInHex.length == 1 ? `0${colorInHex}` : colorInHex;
+  }
+  return result;
 }
+
+console.log(rgbToHex(254,50,50));//ok -> #fe3232
+console.log(rgbToHex(0,0,0));// -> #000000
+console.log(rgbToHex(-248,87,553))
