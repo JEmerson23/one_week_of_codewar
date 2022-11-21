b9eeaf61bf126d6469e0494f9870f43529cc9eb9
JEmerson23 <emersonbezerra174@gmail.com>
2022-11-21 15:18:25 -0300

    Finalizado ultima funcao do ultimo dia do desafio

--- test/rgbToHex.js
+++ test/rgbToHex.js
@@ -4,6 +4,8 @@
  exemplo: (255,255,255) -> FFFFFF
 */
 
+/*
+//[SEM SUCESSO]
 function rgbToHex(r,g,b){
   let result = "";
   for(let color of arguments){
@@ -19,3 +21,21 @@ function rgbToHex(r,g,b){
 console.log(rgbToHex(254,50,50));//ok -> #fe3232
 console.log(rgbToHex(0,0,0));// -> #000000
 console.log(rgbToHex(-248,87,553))
+*/
+
+
+//[minha melhor solução]
+
+function rgbToHex(r,g,b){
+  let result = "";
+  for(let color of arguments){
+    color = color > 255 ? 255 : color < 0 ? 0 : color;
+    let colorInHex = color.toString(16);
+    result += colorInHex.length == 1 ? '0'+colorInHex : colorInHex;
+  }
+  return result;
+}
+
+console.log(rgbToHex(254,50,50));//ok -> fe3232
+console.log(rgbToHex(0,0,0));//ok ->  000000
+console.log(rgbToHex(-248,87,553));//ok -> 0057ff
